554e34819068a75f7fcd13c0e19c5f4d2dfdbc00
Happy Buzaaba <hbuzaaba@gmail.com>
2024-03-25 11:35:22 -0400

    modified the download file

--- script/model_download.py
+++ script/model_download.py
@@ -1,9 +1,16 @@
-from transformers import AutoModel, AutoTokenizer
 
-model_name = "Davlan/afriberta_large"
+from transformers import pipeline
+from transformers import AutoModel, AutoTokenizer, AutoModelForMaskedLM
+
+pipe = pipeline("fill-mask", model="Davlan/afro-xlmr-base")
+
+#model_name = "Davlan/afriberta_large"
 
 # Download and cache model and tokenizer
-model = AutoModel.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+#model = AutoModel.from_pretrained(model_name)
+#tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # The model and tokenizer are now cached in the Hugging Face cache directory
+
+tokenizer = AutoTokenizer.from_pretrained("Davlan/afro-xlmr-base")
+model = AutoModelForMaskedLM.from_pretrained("Davlan/afro-xlmr-base")
